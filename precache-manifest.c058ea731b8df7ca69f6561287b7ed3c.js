2b48aa0f8fb90ef23b57b4ff3559e0b8fc44a0d7
Command <maoxs2@gmail.com>
2020-12-07 14:31:53 +0800

    deploy from ngexplorer-vue

---
+++ precache-manifest.c058ea731b8df7ca69f6561287b7ed3c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c67c49bdafd78ba8be7e79fbab1229",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "20dd41f440d0fa5e7424",
+    "url": "/css/app.0388c220.css"
+  },
+  {
+    "revision": "f9befd30d7c79b62843d",
+    "url": "/css/block.ed1180fa.css"
+  },
+  {
+    "revision": "b8baa56a235d199e91d4",
+    "url": "/css/block~home.9f745daa.css"
+  },
+  {
+    "revision": "e999de5dd978ec6825ed",
+    "url": "/css/chunk-vendors.de86cc28.css"
+  },
+  {
+    "revision": "23c449f8271a6b439d96",
+    "url": "/css/home.9844d247.css"
+  },
+  {
+    "revision": "5924ebd73d76defb539b459fe2218673",
+    "url": "/img/NG.5924ebd7.svg"
+  },
+  {
+    "revision": "e58c532d38b668898417b4a616393778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20dd41f440d0fa5e7424",
+    "url": "/js/app.c18d01dd.js"
+  },
+  {
+    "revision": "f9befd30d7c79b62843d",
+    "url": "/js/block.ee9312a3.js"
+  },
+  {
+    "revision": "b8baa56a235d199e91d4",
+    "url": "/js/block~home.d2b45e04.js"
+  },
+  {
+    "revision": "e999de5dd978ec6825ed",
+    "url": "/js/chunk-vendors.41fe14fc.js"
+  },
+  {
+    "revision": "23c449f8271a6b439d96",
+    "url": "/js/home.67172daf.js"
+  },
+  {
+    "revision": "6c130caf6dab259b71b7485e207d89be",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
